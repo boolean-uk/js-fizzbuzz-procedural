6b82f9de4cf8a588a067962934a00134cf7b921c
Alex Shaw <alexjshaw@gmail.com>
2023-03-06 17:52:03 +0000

    works for Fizz & Buzz, not for FizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,10 +1,20 @@
-const answer = []
+let answer = []
 
 // Write your code below this line
 
-
-
-
+for (let x = 1; answer.length < 15; x++) {
+    if (x % 3 === 0) {
+        answer.push(`Fizz`)
+    } else if (x % 5 === 0) {
+        answer.push(`Buzz`)
+    } else if ((x % 3 === 0) && (x % 5 === 0)) {
+        answer.push('FizzBuzz')
+    } else {
+        answer.push(x)
+    }
+}
+console.log(answer)
 
 // Don't touch the code below this line, we'll cover it later
+
 module.exports = answer
