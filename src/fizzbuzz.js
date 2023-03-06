186308943abb1d61720c86d185f716119f3dba9f
Todd Griffin <todd.griffin61@outlook.com>
2023-03-06 18:01:30 +0000

    fixed case

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -5,15 +5,15 @@ const oneToFifteen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 // Write your code below this line
 
 for (let i = 1; i < 16; i++) {
-    if ((!(i % 3)) && (!(i % 5))) {
-       answer[i-1] = 'fizzbuzz'
-    } else if (!(i % 3)) {
-       answer[i-1] = 'fizz'
-    } else if (!(i % 5)) {
-       answer[i-1] = 'buzz'
-    } else {
-        answer[i-1] = i
-    }
+  if (!(i % 3) && !(i % 5)) {
+    answer[i - 1] = 'FizzBuzz'
+  } else if (!(i % 3)) {
+    answer[i - 1] = 'Fizz'
+  } else if (!(i % 5)) {
+    answer[i - 1] = 'Buzz'
+  } else {
+    answer[i - 1] = i
+  }
 }
 
 console.log(answer)
