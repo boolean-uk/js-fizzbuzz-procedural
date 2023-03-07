41b3dc414a2370361c067f4e048c03aa816eef30
Gabriel Rosengren <gabbe.rosengren@gmail.com>
2023-03-07 15:59:08 +0100

    Completed tests

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,6 +2,18 @@ const answer = []
 
 // Write your code below this line
 
+for(let i = 0; i < 15; i++) {
+    let num = i + 1;
+    if(num % 3 === 0 && num % 5 === 0) {
+        answer[i] = 'FizzBuzz'
+    } else if (num % 3 === 0) {
+        answer[i] = 'Fizz'
+    } else if (num % 5 === 0) {
+        answer[i] = 'Buzz'
+    } else {
+        answer[i] = num
+    }
+}
 
 
 
