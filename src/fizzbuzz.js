2904f8ca1e8f6059191bb5450f8e43deb94a23cc
John Poikilidis <akiskawi@gmail.com>
2023-04-06 16:18:07 +0300

    First Commit

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,10 +1,22 @@
 const answer = []
 
 // Write your code below this line
-
-
-
-
+for (let i = 1; i < 16; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    answer.push('FizzBuzz')
+    continue
+  }
+  if (i % 3 === 0) {
+    answer.push('Fizz')
+    continue
+  }
+  if (i % 5 === 0) {
+    answer.push('Buzz')
+    continue
+  }
+  answer.push(i)
+}
+console.log(answer)
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
