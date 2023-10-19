571f88dcae1ac04a6e37cfdfee7289ec11ea8817
Nikita-vB <n.vanberendonk@gmail.com>
2023-10-19 10:52:55 +0200

    fizzbuzz procedural

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,10 +1,21 @@
 const answer = []
 
 // Write your code below this line
-
-
-
-
+function fizzbuzz() {
+    for (let i = 1; i <= 15; i ++){
+        if (i % 3 == 0 && i % 5 == 0) {
+            answer.push("FizzBuzz")
+        } else if (i % 3 == 0) {
+            answer.push("Fizz")
+        } else if ( i % 5 == 0) {
+            answer.push("Buzz")
+        } else {
+            answer.push(i)
+        }
+    }
+    return answer
+}
+console.log(fizzbuzz())
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
