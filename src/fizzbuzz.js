6da672032567da4023f9490d400f9200a163fc56
oysteinbjo <oystein.bjorkeng@no.experis.com>
2024-02-19 09:54:04 +0100

    done

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,24 @@
+
 const answer = []
-
 // Write your code below this line
-
+function fizzbuzz() {
+    for(let i = 1; i < 16; i++){
+        if(i%15 === 0) {
+            answer.push('FizzBuzz')
+        }
+        else if(i%5===0) {
+            answer.push('Buzz')
+        }
+        else if(i%3===0) {
+            answer.push('Fizz')
+        }
+        else {
+    
+            answer.push(i)
+        }
+    }    
+}
+fizzbuzz()
 
 
 
