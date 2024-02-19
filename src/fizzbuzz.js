31b7d96d794bed0d920f3d0b0d43579d0c4ead94
Gudbrand <gudbrandwd@live.no>
2024-02-19 11:10:20 +0100

    done

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,9 +2,18 @@ const answer = []
 
 // Write your code below this line
 
-
-
-
+for (let i = 0; i < 15; i++)
+{
+    const toAdd = i + 1
+    if (toAdd % 3 === 0 && toAdd % 5 === 0)
+        answer.push('FizzBuzz')
+    else if (toAdd % 3 === 0)
+        answer.push('Fizz')
+    else if (toAdd % 5 === 0)
+        answer.push('Buzz')
+    else
+        answer.push(toAdd)
+}
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
