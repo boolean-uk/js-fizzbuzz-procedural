1dba06661a9ee342d5d0a85268f1cd396f8a819b
Sebank <sebank@gmail.com>
2024-02-20 08:56:16 +0100

    did tasks

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,7 +2,13 @@ const answer = []
 
 // Write your code below this line
 
+for ( let i = 1; i <= 15; i++){
+    let result = '';
+    if ( i % 3 == 0 ) result += 'Fizz';
+    if ( i % 5 == 0 ) result += 'Buzz';
 
+    answer.push(result.length == 0 ? i : result);
+}
 
 
 
