0edce4c0bed3082dd78aabba6ec629f9d6958acb
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-02-20 16:06:21 +0100

    fixed fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,20 @@
 const answer = []
 
 // Write your code below this line
-
+for(let i = 1; i <= 15; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        answer[i-1] = 'FizzBuzz'
+    }
+    else if(i % 3 === 0) {
+        answer[i-1] = 'Fizz'
+    }
+    else if(i % 5 === 0) {
+        answer[i-1] = 'Buzz'
+    }
+    else {
+        answer[i-1] = i
+    }
+}
 
 
 
