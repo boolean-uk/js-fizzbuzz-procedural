c8b6067b215e3e9c72f939c54b50c7ed0359648b
Alexander <alexander.ellnestam@gmail.com>
2024-02-20 11:59:01 +0100

    Done

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -3,7 +3,17 @@ const answer = []
 // Write your code below this line
 
 
-
+for (n = 1; n <= 15; n++) {
+    if (n % 3 === 0 && n % 5 === 0) {
+        answer.push('FizzBuzz')
+    } else if (n % 3 === 0) {
+        answer.push('Fizz')
+    } else if (n % 5 === 0) {
+        answer.push('Buzz')
+    } else {
+        answer.push(n)
+    }
+}
 
 
 // Don't touch the code below this line, we'll cover it later
