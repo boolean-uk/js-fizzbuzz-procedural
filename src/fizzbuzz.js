3e56a050f8679d7dd5676530c5ab411d4d3dda7d
Angus Townsley <angustownsley@gmail.com>
2024-02-20 18:40:58 +0000

    exercise completed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,8 +1,18 @@
 const answer = []
 
 // Write your code below this line
-
-
+for(let i = 1; i < 16; i++) {
+    if (Number.isInteger(i / 3) & Number.isInteger(i / 5)) {
+        answer.push("FizzBuzz")
+    } else if (Number.isInteger(i / 3)) {
+        answer.push("Fizz")
+    } else if (Number.isInteger(i / 5)) {
+        answer.push("Buzz")
+    } else {
+        answer.push(i)
+    }
+}
+console.log(answer)
 
 
 
