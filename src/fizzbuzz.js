ec0841ecbb7be5dc174eca7690fa40914e5e9806
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-02-20 17:45:43 +0100

    pass exercise

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,10 +1,18 @@
 const answer = []
 
 // Write your code below this line
-
-
-
-
+for (let i = 1; i < 16; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    answer.push('FizzBuzz')
+  } else if (i % 5 === 0) {
+    answer.push('Buzz')
+  } else if (i % 3 === 0) {
+    answer.push('Fizz')
+  } else {
+    answer.push(i)
+  }
+}
+console.log(answer)
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
