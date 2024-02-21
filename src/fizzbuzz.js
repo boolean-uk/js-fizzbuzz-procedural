7711175249a8ea53933975156da59cf23ecfbf6d
stp <steventphung6@gmail.com>
2024-02-21 15:48:03 +0100

    Steven Tong Phung

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,9 +2,22 @@ const answer = []
 
 // Write your code below this line
 
+for (let i = 1; i < 16; i++) {
+    let value = ''
+    if (i % 3 === 0) {
+      value += 'Fizz'
+    }
 
+    if (i % 5 === 0) {
+      value += 'Buzz'
+    }
+    
+    if (value === '') {
+      value = i
+    }
 
-
+    answer.push(value)
+  }
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
