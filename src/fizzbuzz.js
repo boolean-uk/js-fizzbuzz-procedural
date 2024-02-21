1364b7e8c23773d21d76bdbea560700c19ae5c22
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-02-21 13:02:21 +0100

    Completed FizzBuzz challenge

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,9 +1,19 @@
 const answer = []
 
 // Write your code below this line
-
-
-
+let index = 0
+for(let i = 1; i < 16; i++){
+    if(i%3 === 0 && i%5=== 0){
+        answer[index] = 'FizzBuzz'
+    } else if(i%3 === 0){
+        answer[index] = 'Fizz'
+    } else if(i%5 === 0){
+        answer[index] = 'Buzz'
+    } else{
+        answer[index] = i
+    }
+    index++
+}
 
 
 // Don't touch the code below this line, we'll cover it later
