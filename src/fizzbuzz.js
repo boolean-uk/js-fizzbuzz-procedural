b25797cb4033bb6f3f37357033b53c6330ca14e3
Martin Johannessen <martin.jodd@gmail.com>
2024-02-21 21:54:39 +0100

    fancy js specific || return

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -5,8 +5,7 @@ for (let i=1; i < 16; i++) {
     let result = ""
     if (i % 3 === 0) result = 'Fizz'
     if (i % 5 === 0) result += 'Buzz'
-    if (result == '') result = i
-    answer.push(result)
+    answer.push(result || i)
 }
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
