f396c3ea4c5beb220f62d5d330e732405772c9ef
Espen Solhaug <espen.solhaug@no.experis.com>
2024-02-21 14:18:12 +0100

    Core finished

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,19 @@
 const answer = []
 
 // Write your code below this line
-
+for(let i = 1; i <= 15; i++) {
+    let value = ""
+    if(i % 3 === 0) {
+        value += "Fizz"
+    }
+    if(i % 5 === 0) {
+        value += "Buzz"
+    }
+    if(value === "") {
+        value = i
+    }
+    answer.push(value)
+}
 
 
 
