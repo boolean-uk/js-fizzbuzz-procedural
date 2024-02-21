6ca9bbd5bb0cd1d415d9c95df2fd5380d7172948
Martin Johannessen <martin.jodd@gmail.com>
2024-02-21 21:50:50 +0100

    Prettier solution

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,21 +2,11 @@ const answer = []
 
 // Write your code below this line
 for (let i=1; i < 16; i++) {
-    let fizz = i % 3 === 0
-    let buzz = i % 5 === 0
-
-    if (fizz && buzz) {
-        answer.push('FizzBuzz')
-    }
-    else if (fizz) {
-        answer.push('Fizz')
-    }
-    else if (buzz) {
-        answer.push('Buzz')
-    }
-    else {
-        answer.push(i)
-    }
+    let result = ""
+    if (i % 3 === 0) result = 'Fizz'
+    if (i % 5 === 0) result += 'Buzz'
+    if (!result) result = i
+    answer.push(result)
 }
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
